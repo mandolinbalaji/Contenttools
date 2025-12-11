8f2fee43e2de28e2a3a248acd49332e30039f3b6
mandolinbalaji <mandolinbala@gmail.com>
2025-12-11 08:55:29 +0000

    Change YouTube to MP3 server to run on port 7773

--- youtube_to_mp3_server.py
+++ youtube_to_mp3_server.py
@@ -149,6 +149,6 @@ def download():
 
 if __name__ == '__main__':
     print("Starting YouTube to MP3 server...")
-    print("Open http://localhost:5000 in your browser")
+    print("Open http://localhost:7773 in your browser")
     print("Press Ctrl+C to stop")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=7773)
